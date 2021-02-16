c9d633e7a2fb7b52944f5bea5fe06363847dcd10
Thuan Luu <69130290+luuvietthuan98@users.noreply.github.com>
2021-02-16 11:01:42 +0700

    Test error regeneratorRuntime when run dist/app.js after build

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import { showMessage } from "./modules/message";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", showMessage);
 
--- src/modules/message.js
+++ src/modules/message.js
@@ -1,3 +1,15 @@
-export const showMessage = (req, res) => {
-  res.send("Message is showed");
+function getMessage(message) {
+  return new Promise((resolve, reject) => {
+    if (!message) return reject("Please enter your message");
+    return resolve(message.toUpperCase());
+  });
+}
+
+export const showMessage = async (req, res) => {
+  try {
+    const message = await getMessage("Message");
+    res.json({ message });
+  } catch (error) {
+    res.json({ error });
+  }
 };
